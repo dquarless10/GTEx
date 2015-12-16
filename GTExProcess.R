26c89761055f25e9764266bfd87437342bd0d73b
DanielEvansLab <danev1@gmail.com>
2015-12-15 23:55:39 -0800

    V6 results and geneGrep function

---
+++ GTExProcess.R
@@ -0,0 +1,19 @@
+##2015-12-15ß
+##parse GTEx results
+##GTEx_Analysis_V6_eQTLs.tar.gz
+##head -n100 Whole_Blood_Analysis.snpgenes > Whole_Blood_Analysis.snpgenes.head100
+
+dat<-read.table("~/Documents/longevity/GTEx/data/Whole_Blood_Analysis.snpgenes.head100",header=T,sep="\t",stringsAsFactors=F)
+
+geneGrep <- function(dat,genes){
+  myRows<-match(genes,dat$gene_name)
+  myRows<-myRows[!is.na(myRows)]
+  dat2<-dat[myRows,c("snp","beta","p_value","ref","alt","gene_name")]
+  return(dat2)
+}
+
+
+myGenes<-c("RP11-693J15.4","RP11-809N8.4","junkNoMatch")
+result<-geneGrep(dat,myGenes)
+
+
